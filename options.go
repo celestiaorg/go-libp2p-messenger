414bda55e06b41a46d37f66a2eec38ad6fdfb629
Wondertan <hlibwondertan@gmail.com>
2022-05-16 10:48:23 +0200

    refactor: rework options to be detached from the Massenger struct

--- options.go
+++ options.go
@@ -2,43 +2,32 @@ package msngr
 
 import (
 	"fmt"
-	"reflect"
 
 	"github.com/libp2p/go-libp2p/core/protocol"
-
-	"github.com/celestiaorg/go-libp2p-messenger/serde"
 )
 
+type options struct {
+	pids []protocol.ID
+}
+
 // Option defines an functional configurability for Messenger.
-type Option func(messenger *Messenger)
+type Option func(*options)
 
 // WithProtocols sets custom protocols for messenger to speak with
 // At least one protocol is required.
 func WithProtocols(pids ...protocol.ID) Option {
-	return func(m *Messenger) {
-		m.pids = pids
+	return func(o *options) {
+		o.pids = append(o.pids, pids...)
 	}
 }
 
-// WithMessageType sets a custom message type for receiving messages.
-// Otherwise Messenger would not know what type to return in Receive.
-func WithMessageType(msg serde.Message) Option {
-	return func(m *Messenger) {
-		tp := reflect.TypeOf(msg)
-		if tp.Kind() == reflect.Ptr {
-			tp = tp.Elem()
-		}
-		m.msgTp = tp
-	}
-}
-
-func (m *Messenger) options(opts ...Option) error {
+func parseOptions(opts ...Option) (*options, error) {
+	o := &options{}
 	for _, opt := range opts {
-		opt(m)
+		opt(o)
 	}
-
-	if m.pids == nil {
-		return fmt.Errorf("messenger: at least one protocol must be given through an option")
+	if o.pids == nil {
+		return nil, fmt.Errorf("messenger: at least one protocol must be given through an option")
 	}
-	return nil
+	return o, nil
 }
